c11cf3b4b7c4ccc6ff10c29cee026102579d4c9a
yussif el-sahey <youssefelsahey@gmail.com>
2023-12-20 11:09:45 +0200

    app colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,31 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesOnboarding1
+  /// assets/images/Onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/Onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/Onboarding2.png
+  static const String imagesOnboarding2 = "assets/images/Onboarding2.png";
+
+  /// Assets for imagesOnboarding3
+  /// assets/images/Onboarding3.png
+  static const String imagesOnboarding3 = "assets/images/Onboarding3.png";
+
+  /// Assets for imagesOnboarding4
+  /// assets/images/Onboarding4.png
+  static const String imagesOnboarding4 = "assets/images/Onboarding4.png";
+
+  /// Assets for imagesOnboarding5
+  /// assets/images/Onboarding5.png
+  static const String imagesOnboarding5 = "assets/images/Onboarding5.png";
+
+  /// Assets for imagesVector
+  /// assets/images/Vector.svg
+  static const String imagesVector = "assets/images/Vector.svg";
+
+  /// Assets for imagesVector1
+  /// assets/images/Vector1.svg
+  static const String imagesVector1 = "assets/images/Vector1.svg";
+}
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Appcolors{
+  static Color primarycolor = const Color(0xffC49E85);
+  static Color secondarycolor = const Color(0xffF8F4F9);
+  static Color deepbrown = const Color(0xff6B4B3E);
+
+  static Color deepgrey = const Color(0xff6F6460);
+  static Color whitegrey = const Color(0xffB4B4B4);
+  static Color black = const Color(0xff333333);
+  static Color grey = const Color(0xffc4c4c4c4);
+  
+
+
+}
